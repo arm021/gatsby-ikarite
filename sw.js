714b25941b92c768d27ccebfcc27c84dce860356
ANDREW REY MICUA <micuaandrew@gmail.com>
2021-01-18 23:34:15 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-70a74b8a32ab89c24265.js"
+    "url": "webpack-runtime-392b0b53f68e3ad980a2.js"
   },
   {
     "url": "framework-44be7afa7abfe592f62f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8fd469d865dcdd094231a887aab965eb"
+    "revision": "5d599db4baf200b0a7f946fa8fd6f7ce"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-959d7e10e315cfbb0346.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "56505c5f4ed0a0078f998d4a68df117b"
+    "revision": "e07a25ad8dfb8f7df3a80ec8ea95f887"
   },
   {
     "url": "polyfill-ff9a188204472ad9b95c.js"
